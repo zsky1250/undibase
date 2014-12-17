58f7cd6bcf9d024a380a9081cb99629e92f0fffa
zwr <zwr_skyon@163.com>
2014-12-17 23:36:58 +0800

    微调了一下HibernatePersistenceProvider

--- src/main/java/com/udf/web/menu/service/MenuService.java
+++ src/main/java/com/udf/web/menu/service/MenuService.java
@@ -3,14 +3,20 @@ package com.udf.web.menu.service;
 import com.udf.web.menu.MenuConfigurer;
 import com.udf.web.menu.bean.Menu;
 import com.udf.web.menu.bean.MenuItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.xml.transform.stream.StreamSource;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,7 +29,13 @@ public class MenuService {
     private MenuConfigurer configurer;
     @Resource
     private Unmarshaller unmarshaller;
+    private static Logger log = LoggerFactory.getLogger(MenuService.class);
 
+
+    @PostConstruct
+    public void init(){
+        loadMenu();
+    }
 
     private Menu loadMenu(){
         try {
@@ -32,11 +44,12 @@ public class MenuService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        log.debug("载入Menu配置文件[name={}]",menu.getName());
         return menu;
     }
 
     public Menu getMenu(){
-        return loadMenu();
+        return menu;
     }
 
 }
